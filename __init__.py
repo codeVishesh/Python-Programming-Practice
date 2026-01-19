70a05bcaf3206f15d5b27cfb6e07ae5c8e34cf67
Vishesh Gupta <vishesh.gupta.jobs@gmail.com>
2026-01-20 01:42:03 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,8 +1,1 @@
-from .jobs import (  # noqa: F401
-    job_details,
-    job_status,
-)
-from .scripts import (  # noqa: F401
-    add_or_update_script,
-    submit_script,
-)
+from __future__ import absolute_import
