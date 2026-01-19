6b4b6c92a5062ce6aebf60f21e41ad8adeab1da2
Vishesh Gupta <vishesh.gupta.jobs@gmail.com>
2026-01-20 01:41:13 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,1 +1,8 @@
-default_app_config = "wooey.apps.WooeyConfig"
+from .jobs import (  # noqa: F401
+    job_details,
+    job_status,
+)
+from .scripts import (  # noqa: F401
+    add_or_update_script,
+    submit_script,
+)
